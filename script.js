89247e94afa0bcfb4ea8a8cb5cc2d64d5c7c9033
Ike2d2 <ike2d2@thefuturesmind.com>
2023-09-18 10:03:48 -0600

    Color Changing Background

--- script.js
+++ script.js
@@ -1,117 +1,174 @@
 "use strict";
 
-let bool = true;
-let number = 420;
-let string = "Hello World";
-let object = {};
-function func() {}
-let sum = 1 + 2;
-let concat = "one " + "two";
+// let bool = true;
+// let number = 420;
+// let string = "Hello World";
+// let object = {};
+// function func() {}
+// let sum = 1 + 2;
+// let concat = "one " + "two";
 // console.log(`[Sum]: ${sum}`);
 // console.log(`[Concat]: ${concat}`);
 
 let container = document.getElementById("container");
 
 for (let i = 0; i < 25; i++) {
-  setTimeout(function () {
-    let frag = document
-      .createDocumentFragment()
-      .appendChild(document.createElement("div"));
-    frag.className = "box";
-    frag.addEventListener("click", pink);
-    container.appendChild(frag);
-  }, 45 * i);
+    setTimeout(function () {
+        let frag = document
+            .createDocumentFragment()
+            .appendChild(document.createElement("div"));
+        frag.className = "box";
+        frag.addEventListener("click", pink);
+        container.appendChild(frag);
+    }, 45 * i);
 }
 
-// let root = document.getRootNode();
-// for(let i=1;i;i++){
-//     root.style.backgroundColor = i % 2 === 0 ? '#f00' : '#00f'
+let body = document.getElementById("body");
+let r = 255;
+let g = 155;
+let b = 155;
+
+setInterval( () => {
+    if (r === 255 && g < 255 && b === 155) {
+        g++;
+    } else if (r > 155 && g === 255) {
+        r--;
+    } else if (r === 155 && g === 255 && b < 255) {
+        b++;
+    } else if (g > 155 && b === 255) {
+        g--;
+    } else if (r < 255 && g === 155 && b === 255) {
+        r++;
+    } else if (r === 255 && b > 155) {
+        b--;
+    }
+    body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+}, 300)
+
+
+// let isaac = {
+//   firstName: "Isaac",
+//   lastName: "Stark",
+//   age: 23,
+//   address: null,
+// };
+
+// let santa = {
+//   firstName: "Santa",
+//   lastName: "Claus",
+//   age: 23,
+//   address: "The North Pole",
+// };
+
+// function locate(person) {
+//   person.address === null
+//     ? console.log(`${person.firstName} is homeless`)
+//     : console.log(`${person.firstName} lives at ${person.address}`);
 // }
-
-let isaac = {
-  firstName: "Isaac",
-  lastName: "Stark",
-  age: 23,
-  address: null,
-};
-
-let santa = {
-  firstName: "Santa",
-  lastName: "Claus",
-  age: 23,
-  address: "The North Pole",
-};
-
-function locate(person) {
-  person.address === null
-    ? console.log(`${person.firstName} is homeless`)
-    : console.log(`${person.firstName} lives at ${person.address}`);
-}
 
 // locate(isaac);
 // locate(santa);
 
-function quadArea(width, height = width) {
-  console.log("Area of quadrilateral is: " + width * height);
-}
+// function quadArea(width, height = width) {
+//     console.log("Area of quadrilateral is: " + width * height);
+// }
 
 // quadArea(4, 7);
 // quadArea(18, 213124);
 // quadArea(3);
 
-function calculator() {
-  let first = prompt("Enter first number:");
-  let second = prompt("Enter second number");
-}
+// function calculator() {
+//     let first = prompt("Enter first number:");
+//     let second = prompt("Enter second number");
+//     alert(first + second);
+// }
 
 function fizzbuzz() {
-  let fizzbuzzing = true;
-  do {
-    let x = prompt("Enter Number");
-    if (parseInt(x)) {
-      if (x % 15 === 0) {
-        return "fizzbuzz";
-        fizzbuzzing = false;
-      } else if (x % 5 === 0) {
-        return "buzz";
-        fizzbuzzing = false;
-      } else if (x % 3 === 0) {
-        return "fizz";
-        fizzbuzzing = false;
-      } else {
-        return x;
-        fizzbuzzing = false;
-      }
-    } else {
-      alert("Please Enter A Number");
-    }
-  } while (fizzbuzzing);
+    let fizzbuzzing = true;
+    do {
+        let x = prompt("Enter Number");
+        if (parseInt(x)) {
+            if (x % 15 === 0) {
+                return "fizzbuzz";
+                fizzbuzzing = false;
+            } else if (x % 5 === 0) {
+                return "buzz";
+                fizzbuzzing = false;
+            } else if (x % 3 === 0) {
+                return "fizz";
+                fizzbuzzing = false;
+            } else {
+                return x;
+                fizzbuzzing = false;
+            }
+        } else {
+            alert("Please Enter A Number");
+        }
+    } while (fizzbuzzing);
 }
 
 function pink(e) {
-  e.target.classList.toggle("pink");
-  e.target.innerText = e.target.innerText === "" ? fizzbuzz() : "";
+    e.target.classList.toggle("pink");
+    e.target.innerText = e.target.innerText === "" ? fizzbuzz() : "";
 }
 
 function checkTime() {
-  let time = document.getElementById("time");
+    let time = document.getElementById("time");
 
-  if (!isNaN(time.value)) {
-    let display = document.getElementById("display");
-    display.innerText = rangeCheck(time.value);
-  } else {
-    alert("Please Enter A Number");
-  }
+    if (!isNaN(time.value)) {
+        let display = document.getElementById("display");
+        display.innerText = rangeCheck(time.value);
+    } else {
+        alert("Please Enter A Number");
+    }
 }
 
 function rangeCheck(x) {
-  console.log("checking");
-  console.log(x);
-  if (x >= 0 && x <= 12) return "Good Morning";
-  else if (x >= 13 && x <= 16) return "Good Afternoon";
-  else if (x >= 17 && x <= 19) return "Good Evening";
-  else if (x >= 20 && x <= 24) return "Good Night";
-  else "help";
+    console.log("checking");
+    console.log(x);
+    if (x >= 0 && x <= 12) return "Good Morning";
+    else if (x >= 13 && x <= 16) return "Good Afternoon";
+    else if (x >= 17 && x <= 19) return "Good Evening";
+    else if (x >= 20 && x <= 24) return "Good Night";
+    else "help";
 }
 
 document.getElementById("checkTime").addEventListener("click", checkTime);
+
+// I know a few digits of PI
+// const PI = 3.14159265358979323846264338327950288419716939936;
+// const num = PI.toFixed(2);
+// const num2 = PI.toPrecision(5);
+// console.log(num);
+// console.log(num2);
+// console.log(Math.PI);
+
+// let mill = 1e6;
+// console.log(mill.toExponential())
+
+// const PI = Math.PI;
+// let min = Math.min(1, 2, 3, 233, 45, 78, 3000);
+// let pow = Math.pow(5, 3);
+// console.log(pow);
+// pow = 5 ** 3;
+// console.log(pow)
+// ** is cooler than Math.pow()
+
+// Dynamic Variables because swag
+for (let i = 1; i < 4; i++) {
+    window[`num` + i] = Math.floor(Math.random() * 30) + 1;
+}
+
+console.log(`num1: ${num1}`);
+console.log(`num2: ${num2}`);
+console.log(`num3: ${num3}`);
+
+// let s = "'single'"
+// let d = '"double"'
+// let b = "`backtick`"
+// console.log(s,d,b)
+
+// let speech = `Martin Luther King Jr. said, "I have a dream..."`
+// console.log(speech);
+// let tip = "We can escape quotes by using \\"
+// console.log(tip);
